dc797845db5601a673506fc36ef7eda729ca21d8
feel2code <feel2code@proton.me>
2024-02-19 13:14:42 +0400

    moved to neovim

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # installing pkgs that need to comfortable work
-pacman -S git vim terminator i3 brightnessctl playerctl feh flameshot fonts-font-awesome emacs ncdu mc ranger htop bat liquidpromt keepassxc
+pacman -S terminator git vim neovim i3 brightnessctl playerctl feh flameshot fonts-font-awesome ncdu mc ranger htop bat liquidpromt keepassxc firefox
 # adding user to video group to grant access for brightness control
 sudo usermod -a -G video ${USER}
