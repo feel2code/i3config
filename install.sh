1f98a4d91aeb893b9c383e9e9c0b4b88d9b5e94d
feel2code <feel2code@proton.me>
2024-02-21 22:17:29 +0400

    added patch for center dmenu, minor fixes

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 # installing pkgs that need to comfortable work
-pacman -S terminator git vim neovim i3 brightnessctl playerctl feh flameshot fonts-font-awesome ncdu mc ranger htop bat liquidpromt keepassxc firefox
+pacman -S terminator git vim neovim i3 brightnessctl playerctl feh flameshot fonts-font-awesome ncdu mc ranger htop liquidpromt keepassxc firefox
 # adding user to video group to grant access for brightness control
 sudo usermod -a -G video ${USER}
+sudo pacman -R dmenu
+git clone https://git.suckless.org/dmenu
+cp dmenu-center-format.patch dmenu/dmenu-center-format.patch
+cd dmenu
+path -i dmenu-center-format.patch
+make && sudo make install
+cd ..
+rm -rf dmenu
