66533fd12dd2702e0d522e858907698ef39dcc83
feel2code <feel2code@proton.me>
2024-02-25 23:58:44 +0400

    moved to xterm

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # installing pkgs that need to comfortable work
-pacman -S terminator git vim neovim i3 brightnessctl playerctl feh flameshot fonts-font-awesome ncdu mc ranger htop liquidpromt keepassxc firefox
+sudo pacman -S xterm git vim neovim i3 w3m wget curl brightnessctl playerctl feh mpv flameshot fonts-font-awesome ncdu mc ranger htop liquidpromt keepassxc firefox picom
 # adding user to video group to grant access for brightness control
 sudo usermod -a -G video ${USER}
 sudo pacman -R dmenu
